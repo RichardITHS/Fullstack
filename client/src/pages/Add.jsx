7de5aee312958b756aa52a892d04b7375c160d35
Navigoo <81353827+Navigoo@users.noreply.github.com>
2023-05-02 13:43:37 +0200

    add

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,8 +1,40 @@
+//Import av axios då vi på denna sidan kommer att göra en POST
+import axios from "axios"
+//När vi lagt till en ny bok så updateras vårt "book" state (vår array), därav använder vi usestate
+import {useState} from "react"
+import { useNavigate } from "react-router-dom"
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+    const [book, setBook] = useState({
+        title: "",
+        about: "",
+        price: null,
+        cover: ""
+    })
+    const [error, setError]=useState(false);
 
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setBook((prev) => ({...prev, [e.target.name]: e.target.value}));
+
+    }
+    const handleClick = async (e) => {
+        e.preventDefault();
+        try{
+            await axios.post("http://localhost:8800/books", book);
+            navigate("/");
+        }catch (err) {
+            console.log(err)
+            setError(true)
+        }
+    }
+    return (
+        <div className="form">
+            <h1>Add a new book</h1>
+            <input type="text" placeholder="Book title" name="title" onChange={handleChange}/>
+            
+        </div>
+    )
+}
 export default Add
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,18 +1,53 @@
 import {useEffect, useState} from "react"
+import axios from "axios";
+import {Link} from "react-router-dom"
 
 //Usestate som hanterar böckerna som vi sedan kan arbeta med
 const Books = () => {
     const [books, setBooks] = useState([])
-    
+
     //Inhämtar direkt information vid sidladdning från databasen
     useEffect(() => {
         const fetchAllBooks = async () => {
             try {
-                const res = await.get("http://localhost:8800/books");
-
+                const res = await axios.get("http://localhost:8800/books");
+                setBooks(res.data);
+            } catch (err) {
+                console.log(err)
             }
-        }
-    })
+        };
+        fetchAllBooks();
+    }, [])
+
+    const handleDelete = async (id) => {
+       try {
+        await axios.delete(`http://localhost:8800/books/${id}`)
+        window.location.reload()
+       }catch (err) {
+        console.log(err)
+       }
+    }
+
+    return (
+        <div>
+            <h1>Richard´s Book shop</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div key={book.id} className="book">
+                        <img src={book.cover} alt="" className="profile-photo" />
+                        <h3>{book.title}</h3>
+                        <p>{book.about}</p>
+                        <span>{book.price} Kr</span>
+                        <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
+                    </div>
+
+
+
+                ))}
+            </div>
+            <button className="addHome"><Link to="/add">Add new book</Link></button>
+        </div>
+    )
 }
 
 
